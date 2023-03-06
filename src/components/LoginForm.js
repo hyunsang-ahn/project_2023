0f9bf9d721b77e80740ccff4290862081159edb3
hyunsang-ahn <happylovetkd@naver.com>
2023-03-06 22:41:13 +0000

    login logout

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,9 @@
 
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useForm } from "react-hook-form";
 import axios from 'axios';
 const LoginForm = () => {
+    const [loginStatus, setLoginStatus] = useState(false)
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
     useEffect(() => {
         loginChk()
@@ -18,8 +19,15 @@ const LoginForm = () => {
                 ...formData
             })
 
-            console.log("response >>", response)
-        } catch (err) {
+            if (response.data === "") {
+                console.log("비로그인 상태입니다.")
+
+            } else {
+                console.log("로그인 상태입니다.")
+                setLoginStatus(true)
+
+            }    
+            } catch (err) {
             console.log("Error >>", err);
         }
     }
@@ -28,7 +36,15 @@ const LoginForm = () => {
         try {
             const response = await axios.get('/api/loginChk')
 
-            console.log("response >>", response)
+            console.log("response >>", response.data)
+            if (response.data === "") {
+                console.log("비로그인 상태입니다.")
+
+            } else {
+                console.log("로그인 상태입니다.")
+                setLoginStatus(true)
+
+            }
         } catch (err) {
             console.log("Error >>", err);
         }
@@ -48,6 +64,7 @@ const LoginForm = () => {
 
 
             <input type="submit" />
+            {loginStatus && <button>로그아웃하기</button>}
         </form>
     );
 }
