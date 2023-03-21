91785e6ac4e131982a432b33c04bf61e0858923e
hyunsang-ahn <happylovetkd@naver.com>
2023-03-22 07:24:41 +0900

    local connet -mongo

--- server/db.js
+++ server/db.js
@@ -39,40 +39,114 @@
 // });
 
 
+// const { createTunnel } = require('tunnel-ssh');
+
+
+
+// const port = 27017;
+
+// const tunnelOptions = {
+//   autoClose: true
+// };
+// const serverOptions = {
+//   host: '127.0.0.1',
+//   port: 27017
+// }
+// const sshOptions = {
+//   host: process.env.SSH_HOST,
+//   port: process.env.SSH_PORT,
+//   username: process.env.SSH_USER,
+//   password: process.env.SSH_PASSWORD,
+// };
+// const forwardOptions = {
+//   srcAddr: '0.0.0.0',
+//   srcPort: port,
+//   dstAddr: '127.0.0.1',
+//   dstPort: port
+// };
+
+// createTunnel(tunnelOptions, serverOptions, sshOptions, forwardOptions).
+//   then(([server, conn], error) => {
+//     console.log('server=====================', server)
+//     server.on('error', (e) => {
+//       console.log(e);
+//     });
+
+//     conn.on('error', (e) => {
+//       console.log(e);
+//     });
+//   });
+
 const { createTunnel } = require('tunnel-ssh');
 
+// const port = 27017;
 
+// const tunnelOptions = {
+//   autoClose: true
+// };
+// const serverOptions = {
+//   port: port
+// };
+// const sshOptions = {
+//   host: process.env.SSH_HOST,
+//   port: process.env.SSH_PORT,
+//   username: process.env.SSH_USER,
+//   password: process.env.SSH_PASSWORD,
+// };
+// const forwardOptions = {
+//   srcAddr: '0.0.0.0',
+//   srcPort: port,
+//   dstAddr: '127.0.0.1',
+//   dstPort: port
+// };
 
-const port = 27017;
+// let [server, conn] = await createTunnel(tunnelOptions, serverOptions, sshOptions, forwardOptions);
 
-const tunnelOptions = {
-  autoClose: true
-};
-const serverOptions = {
-  host: '127.0.0.1',
-  port: 27017
+// server.on('connection', (connection) => {
+//   console.log('new connection');
+// });
+
+async function connectToMongoDB() {
+  const tunnelOptions = {
+    host: process.env.SSH_HOST,
+    port: process.env.SSH_PORT,
+    username: process.env.SSH_USER,
+    password: process.env.SSH_PASSWORD,
+    dstPort: 27017,
+    localPort: 27017
+  };
+
+  const serverOptions = {
+    host: 'localhost',
+    port: 27017,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
+  const sshOptions = {
+    keepAlive: true,
+    readyTimeout: 20000,
+  };
+
+  const forwardOptions = {
+    srcPort: 27017,
+    dstHost: 'localhost',
+    dstPort: 27017
+  };
+
+  const [server, conn] = await createTunnel(tunnelOptions, serverOptions, sshOptions, forwardOptions);
+
+  // MongoDB에 연결
+  const db = conn.db('database-name');
+  const collection = db.collection('collection-name');
+  const result = await collection.find().toArray();
+
+  // 결과 출력
+  console.log('result==========================', result);
+
+  // MongoDB 및 SSH 터널 닫기
+  await conn.close();
+  await server.close();
 }
-const sshOptions = {
-  host: process.env.SSH_HOST,
-  port: process.env.SSH_PORT,
-  username: process.env.SSH_USER,
-  password: process.env.SSH_PASSWORD,
-};
-const forwardOptions = {
-  srcAddr: '0.0.0.0',
-  srcPort: port,
-  dstAddr: '127.0.0.1',
-  dstPort: port
-};
 
-createTunnel(tunnelOptions, serverOptions, sshOptions, forwardOptions).
-  then(([server, conn], error) => {
-    console.log('server=====================', server)
-    server.on('error', (e) => {
-      console.log(e);
-    });
-
-    conn.on('error', (e) => {
-      console.log(e);
-    });
-  });
+connectToMongoDB().catch(console.error);
