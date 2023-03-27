ba49e97f06acf4a0d490fa8d12c37c0e893519a1
hyunsang-ahn <happylovetkd@naver.com>
2023-03-28 07:09:38 +0900

    crypto를 이용한 비밀번호 해싱 salt 및 로그인 시 검증과정 추가

--- server/passport.js
+++ server/passport.js
@@ -1,5 +1,7 @@
 const passport = require("passport");
 const LocalStrategy = require('passport-local').Strategy;
+const client = require('./db')
+const crypto = require('crypto');
 
 
 
@@ -24,19 +26,48 @@ module.exports = () => {
     usernameField: 'email',
     passwordField: 'password',
     passReqToCallback: true
-  }, (req, userId, password, done) => {
+  }, async (req, userId, password, done) => {
     console.log('passport의 local-login : ', userId, password)
 
-    if (userId != 'test' || password != '12345') {
+    const makePasswordHashed = (userId, plainPassword) =>
+      new Promise(async (resolve, reject) => {
+        // salt를 가져오는 부분은 각자의 DB에 따라 수정
+        const salt = await client.db('project').collection('users')
+          .findOne({
+            email: userId,
+
+          })
+          .then((result) => result.salt);
+        console.log('salt=======================', salt)
+        crypto.pbkdf2(plainPassword, salt, 9999, 64, 'sha512', (err, key) => {
+          if (err) reject(err);
+          resolve(key.toString('base64'));
+        });
+      });
+
+    const hashed_password = await makePasswordHashed(userId, password);
+
+    console.log('hashed_password=======================', hashed_password)
+
+    const user_data = await client.db('project').collection('users')
+      .findOne({
+        email: userId,
+
+      })
+
+    console.log('user_data=======================', user_data)
+    if (user_data.password === hashed_password) {
+      console.log('비밀번호 일치!')
+      return done(null, {
+        userId: userId,
+        password: password
+      })
+    } else {
       console.log('비밀번호 불일치!')
       return done(null, false, req.flash('loginMessage', '비밀번호 불일치!'))
     }
 
-    console.log('비밀번호 일치!')
-    return done(null, {
-      userId: userId,
-      password: password
-    })
+
   }))
 
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -7,6 +7,7 @@ const path = require('path');
 const router = express.Router(); // 라우터 분리
 const mongoose = require('mongoose')
 const client = require('../db')
+const crypto = require('crypto');
 
 router.post('/login', passport.authenticate('local', {
     successRedirect: '/custom-api/success',
@@ -46,12 +47,37 @@ router.get('/logout', function (req, res, next) {
 
 router.post('/register', async (req, res, next) => {
     console.log('req========================', req.body)
-
     const { email, name, password } = req.body
+
+    const createSalt = () =>
+        new Promise((resolve, reject) => {
+            crypto.randomBytes(64, (err, buf) => {
+                if (err) reject(err);
+                resolve(buf.toString('base64'));
+            });
+        });
+    const createHashedPassword = (plainPassword) =>
+        new Promise(async (resolve, reject) => {
+            const salt = await createSalt();
+            console.log('salt==========================', salt)
+            crypto.pbkdf2(plainPassword, salt, 9999, 64, 'sha512', (err, key) => {
+                if (err) reject(err);
+                resolve({ crypto_pw: key.toString('base64'), salt });
+            });
+        });
+    const { crypto_pw, salt } = await createHashedPassword(password);
+    console.log('crypto_pw===================', crypto_pw)
+    console.log('salt===================', salt)
+
     const result = await client.db('project').collection('users').insertOne({
         email: email,
         name: name,
-        password: password,
+        password: crypto_pw,
+        salt: salt,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        login_method: 'own'
+
     })
 
     console.log('result=========================', result)
